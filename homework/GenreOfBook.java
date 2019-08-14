efc52d736d9ce2e90cba115a4230131aa5c10a49
Shabanova Dinara <dinara.shabanova@yandex.ru>
2019-08-14 13:04:42 +0300

    Realixation interface for books and authors

--- homework/GenreOfBook.java
+++
@@ -1,9 +0,0 @@
-package ru.yusdm.stud.lesson_4_oop_continue.homework;
-
-/**
- * Created by Dinara Shabanova on 08.08.2019.
- */
-public enum GenreOfBook {
-    ENCYCLOPEDIA, NOVEL, FANTASY, ADVENTURE,
-    FAIRY_TALE, POEM
-}
